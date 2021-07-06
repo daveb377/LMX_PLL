249d7c773140c4c9b781e50182e38c6913237159
David Bengtson <david.bengtson@gmail.com>
2021-07-05 22:16:41 -0400

    Update 2021-07-05
    Finished Schematic enough to get a first cut pulled into the layout.
    
    Also finished PLL_Calculations.py enought to get loop filter component footprints nailed. Still needs more work, but ongoing. I'm using Spyder as a Python IDE.

--- LoopFilterCalcs/PLL_Calculations.py
+++ LoopFilterCalcs/PLL_Calculations.py
@@ -10,9 +10,60 @@ loop filter and performance parameters of a PLL based on the LMX2332
 import math
 from icecream import ic
 
-# Constants
+# 
 
-Fref=16.8e6
+# =============================================================================
+# 
+#                          ---------)-------------------
+#                                   |         |
+#                                   |        --- C2
+#                                   |        ---
+#                                   |         |
+#                                   |         |
+#                               C1 ---        |
+#                                  ---        |
+#                                   |        .-. R2
+#                                   |        | |
+#                                   |        | |
+#                                   |        '-'
+#                                   |         |
+#                                   '----------
+#                                        |
+#                                       ===
+#                                       GND
+#  
+# =============================================================================
+
+
+# Data Check from Banerjee SNAA106C
+# Ch 38 Appendix A
+
+
+# Fref=50e3
+# ChannelSpacing=25e3
+# Fmin = 380e6
+# NChannels = 2401
+
+
+# # VCO
+# VCOMin = 1930e6
+# VCOMax = 1990e6
+# VTuneMin = 0.5
+# VTuneMax = 1.5
+
+# # PLL Consts
+# Kpd = 1e-3 # in A
+# LoopBW = 10e3
+# Gamma = 1.024
+# PMDeg=49.2
+
+# Nmin=VCOMin/Fref
+# Nmax=VCOMax/Fref
+
+# 16.8 MHz TCXO
+
+Fref=16.e6
+
 ChannelSpacing=25e3
 Fmin = 380e6
 NChannels = 2401
@@ -25,7 +76,7 @@ VTuneMin = 0.5
 VTuneMax = 4.5
 
 # PLL Consts
-Kpd = 1e-3 # in A
+Kpd = 4.5e-3 # in A
 LoopBW = 10e3
 Gamma = 1.024
 PMDeg=49.2
@@ -36,6 +87,11 @@ PMDeg=49.2
 Nmin = Fmin/ChannelSpacing
 Nmax = Nmin + NChannels
 Ndesign=math.sqrt(Nmin*Nmax)
+
+# Reference Divider
+Rdesign = Fref/ChannelSpacing
+ic(Rdesign)
+
 
 Kvco = (VCOMax-VCOMin)/(VTuneMax-VTuneMin)
 ic(Nmin)
@@ -55,12 +111,22 @@ ic(PMRad)
 T1P1= (1+Gamma)*math.tan(math.radians(PMDeg))
 T1P2 = math.sqrt(((1+Gamma)**2)*(math.tan(math.radians(PMDeg))**2)+4*Gamma)
 T1 =(T1P2-T1P1)/(2*wc)
+ic(T1)
+
 # Eq 38.17
 T2 = Gamma/((wc**2)*T1)
-
+ic(T2)
 # Eq 38.8
 A0P1 = ((Kpd*Kvco)/(Ndesign*(wc**2)))*math.sqrt((1+(wc*T2)**2)/(1+(wc*T1)**2))
+ic(A0P1)
 
+C1 = A0P1*(T1/T2)
+C2 = A0P1 - C1
+R2 = T2/C2
+
+ic(C1)
+ic(C2)
+ic(R2)
 
 
 
